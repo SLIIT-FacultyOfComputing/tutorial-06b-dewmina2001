275d6d493db3ba02c1a956104d31c6be7228245d
dewmina2001 <110800029+dewmina2001@users.noreply.github.com>
2022-09-21 23:56:16 +0530

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,17 @@
 #include "Student.h"
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int studentIdn, char n[20]) {
+  strcpy(name, n);
+  studentId = studentIdn;
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
+  cout<< "Student Name = " << name<<endl;
+  cout<< "Student ID = "<<studentId;
   
 }
